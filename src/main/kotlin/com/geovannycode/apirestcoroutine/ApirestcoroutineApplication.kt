a905e9972649ea7f2994446351e5d61615fc799d
Geovanny Mendoza <geovanny0401@gmail.com>
2023-05-12 00:45:52 -0500

    [V-1]Estructura de proyecto

---
+++ src/main/kotlin/com/geovannycode/apirestcoroutine/ApirestcoroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.geovannycode.apirestcoroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApirestcoroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApirestcoroutineApplication>(*args)
+}
---
+++ src/test/kotlin/com/geovannycode/apirestcoroutine/ApirestcoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.geovannycode.apirestcoroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApirestcoroutineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
